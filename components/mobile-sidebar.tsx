30e9e7326d208d9131724313f7047f6137b2a6c7
summer030 <karaage4projects@outlook.com>
2023-08-04 21:06:45 +0800

    fix: hydration failure

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
 const MobileSideBar = () => {
+  // fix hydration failure
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <Sheet>
       <SheetTrigger>
